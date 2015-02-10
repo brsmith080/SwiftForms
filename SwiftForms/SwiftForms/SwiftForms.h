20d1e2253681ec69f317336572aff8e49d802104
Brandon Smith <brsmith080>
2015-02-09 22:32:06 -0600

    added a workspace and split the project into example stuff and a dynamic library. changed a lot of stuff in the library to be public

---
+++ SwiftForms/SwiftForms/SwiftForms.h
@@ -0,0 +1,19 @@
+//
+//  SwiftForms.h
+//  SwiftForms
+//
+//  Created by Brandon Smith on 2/9/15.
+//  Copyright (c) 2015 Brandon Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftForms.
+FOUNDATION_EXPORT double SwiftFormsVersionNumber;
+
+//! Project version string for SwiftForms.
+FOUNDATION_EXPORT const unsigned char SwiftFormsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftForms/PublicHeader.h>
+
+
